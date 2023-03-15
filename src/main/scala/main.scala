a30c07d0f684ed8fc87ffdc49176920169dd80ff
Adam-Vandervorst <adam.vandervorst@gmail.com>
2023-03-15 11:52:57 +0100

    Allow interpreting a JSON list as multiple samples

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -14,15 +14,20 @@ val inferrer = JsonSchemaInferrer
   .newBuilder
   .setSpecVersion(SpecVersion.DRAFT_06)
   .addFormatInferrers(FormatInferrers.email, FormatInferrers.dateTime, FormatInferrers.ip)
-  .setAdditionalPropertiesPolicy(AdditionalPropertiesPolicies.notAllowed)
+  .setAdditionalPropertiesPolicy(AdditionalPropertiesPolicies.noOp())
   .setExamplesPolicy(ExamplesPolicies.useFirstSamples(3))
   .setRequiredPolicy(RequiredPolicies.nonNullCommonFields)
   .addEnumExtractors(EnumExtractors.validEnum(classOf[Month]), EnumExtractors.validEnum(classOf[DayOfWeek]))
   .setArrayLengthFeatures(util.EnumSet.allOf(classOf[ArrayLengthFeature]))
   .build
 
-@main def example(filename: String) =
+@main def example(filename: String, as_multiple: Boolean) =
   val f = new File(filename)
   val contents = mapper.readTree(f)
-  val result = inferrer.inferForSample(contents)
+  val result =
+    if as_multiple && contents.isArray then
+      val list: java.util.ArrayList[JsonNode] = new java.util.ArrayList()
+      contents.elements().forEachRemaining(list.add(_))
+      inferrer.inferForSamples(list)
+    else inferrer.inferForSample(contents)
   println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(result))
